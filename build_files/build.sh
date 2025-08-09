76470d4d9d2aca6ed96c1b87cb9d8240cd6e1b92
Chris <1905121+lauretano@users.noreply.github.com>
2025-08-09 00:03:39 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -32,10 +32,10 @@ dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-uki-
   kernel-headers kernel-tools kernel-tools-libs
 dnf5 -y versionlock delete kernel kernel-core kernel-modules \
   kernel-modules-core kernel-tools kernel-tools-libs
-dnf5 -y install kernel kernel-core \
+dnf5 -y install --repo=copr:copr.fedorainfracloud.org:group_asahi:kernel kernel kernel-core \
   kernel-modules kernel-modules-core kernel-tools kernel-tools-libs
 
-#dnf5 -y --repo=copr:copr.fedorainfracloud.org:@asahi:mesa install mesa spirv-llvm-translator18 virglrenderer
+dnf5 -y --repo=copr:copr.fedorainfracloud.org:group_asahi:mesa install mesa spirv-llvm-translator18 virglrenderer
 dnf5 -y install mesa-asahi-24.08-flatpak
 #dnf5 -y install arm-trusted-firmware crust-firmware uboot-tools
 dnf5 -y install asahi-audio asahi-scripts m1n1
